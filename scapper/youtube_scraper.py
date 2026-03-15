da94eb76a377741f55ae0fbe3622e5c4dc82e199
r1ddh118 <riddhipoddar3@gmail.com>
2026-03-15 13:44:23 +0530

    added chunking, tagging and trust score from different py

--- scapper/youtube_scraper.py
+++ scapper/youtube_scraper.py
@@ -5,9 +5,14 @@ import requests
 from bs4 import BeautifulSoup
 from langdetect import detect
 
+from utils.tagging import extract_tags
+from utils.chunking import chunk_text
+from scoring.trust_score import calculate_trust
+
 def scrape_youtube(video_id):
     url = f"https://www.youtube.com/watch?v={video_id}"
     headers = {"User-Agent": "Mozilla/5.0"}
+    
     r = requests.get(url, headers = headers)
     soup = BeautifulSoup(r.text, "html.parser")
     
@@ -17,31 +22,42 @@ def scrape_youtube(video_id):
     date_tag = soup.find("meta", itemprop="datePublished")
     published_date = date_tag["content"] if date_tag else "Unknown"
     
-    desc_tag = soup.find("meta", {"name":"description"})
-    description = desc_tag["content"] if desc_tag else ""
-    
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        chunks = [t["text"] for t in transcript]
-        full_text = " ".join(chunks)
-    except:
-        chunks = [description]
-        full_text = description
+    publish_year = None
+    if published_date != "Unknown":
+        try:
+            publish_year = int(published_date[:4])
+        except:
+            pass
         
     try:
-        language = detect(full_text)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        paragraphs = [t["text"] for t in transcript]
+    except:
+        paragraphs = ["Transcript unavailable"]
+
+    full_text = " ".join(paragraphs)
+
+        
+    try:
+        language = detect(full_text) if full_text else "unknown"
     except:
         language = "unknown"
     
+    topic_tags = extract_tags(full_text)
+    
+    content_chunks = chunk_text(paragraphs)
+
+    trust_score = calculate_trust(url=url, author=channel_name, source_type="youtube", publish_year=publish_year, soup=soup, text=full_text)
 
     return {
         "source_url": url,
         "source_type": "youtube",
         "author": channel_name,
         "published_date": published_date,
+        "publication_year": publish_year,
         "language": language,
         "region": "",
-        "topic_tags": [],
-        "trust_score": "",
-        "content_chunks": chunks
+        "topic_tags": topic_tags,
+        "trust_score": trust_score,
+        "content_chunks": content_chunks
     }
