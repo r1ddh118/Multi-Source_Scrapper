bfa53f1e3891ecda8d67130a9ab7c5993b6a7102
r1ddh118 <riddhipoddar3@gmail.com>
2026-03-15 13:32:47 +0530

    added chunking, tagging and trust score from different py

--- scapper/blog_scraper.py
+++ scapper/blog_scraper.py
@@ -4,38 +4,48 @@ import requests
 from bs4 import BeautifulSoup
 from langdetect import detect
 
+from utils.tagging import extract_tags
+from utils.chunking import chunk_text
+from scoring.trust_score import calculate_trust
+
 def scrape_blog(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
-    
-    if soup.title:
-        title = soup.title.string    
-    else:
-        title = ""
         
     author = ""
     
     if soup.find("meta", {"name":"author"}):
         author = soup.find("meta", {"name":"author"})["content"]
-        
-    date = ""
-    
+
+    published_date = ""
+
     if soup.find("time"):
-        data = soup.find("time").text
-        
-    paragraphs = [p.text for p in soup.find_all("p")]
-    content = " ".join(paragraphs)
+        published_date = soup.find("time").text
     
-    language = detect(content) if content else "unknown"
+    if published_date:
+        try:
+            publish_year = int(published_date[-4:])
+        except:
+            pass
+
+    paragraphs = [p.text for p in soup.find_all("p")]
+    full_text = " ".join(paragraphs)
+
+    language = detect(full_text) if full_text else "unknown"
+
+    topic_tags = extract_tags(full_text)
+    content_chunks = chunk_text(paragraphs)
+    
+    trust_score = calculate_trust(url, author, "blog", publish_year, soup, full_text)
     
     return {
         "source_url": url,
         "source_type": "blog",
         "author": author,
-        "published_date":date,
+        "published_date": published_date,
         "language": language,
         "region": "",
-        "topic_tags": [],
-        "trust_score" : "",
-        "content_chunks": paragraphs
+        "topic_tags": topic_tags,
+        "trust_score": trust_score,
+        "content_chunks": content_chunks
     }
