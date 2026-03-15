770ad5373ac4bc567ec6e77e26ce95bb0ca6e1f8
r1ddh118 <riddhipoddar3@gmail.com>
2026-03-15 13:18:35 +0530

    added the initial version of chunking

--- utils/chunking.py
+++ utils/chunking.py
@@ -1,1 +1,8 @@
 # chunking.py
+
+def chunk_text(paragraphs, size = 3):
+    chunks = []
+    for i in range(0, len(paragraphs), size):
+        chunks = " ".join(paragraphs[i:i + size])
+        chunks.append(chunk)
+    return chunks
