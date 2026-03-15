5d1164821c982b923dc1d00d93ace410e1a0f00e
r1ddh118 <riddhipoddar3@gmail.com>
2026-03-15 13:00:43 +0530

    initial version of pubmed added

--- scapper/pubmed_scaper.py
+++ scapper/pubmed_scaper.py
@@ -1,1 +1,30 @@
 # pubmed_scraper.py
+
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_pubmed(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    if soup.find("h1"):
+        title = soup.find("h1").text
+    else:
+        title = ""
+
+    author = ", ".join([a.text for a in soup.find_all("a", {"class": "full-name"})])
+    
+    published_date = soup.find("meta", {"name": "date"})["content"] if soup.find("meta", {"name": "date"}) else ""
+    abstract = soup.find("div", {"class": "abstract"}).text if soup.find("div", {"class": "abstract"}) else ""
+
+    return {
+        "source_url": url,
+        "source_type": "pubmed",
+        "author": author,
+        "published_date": published_date,
+        "language": "unknown",
+        "region": "",
+        "topic_tags": [],
+        "trust_score": "",
+        "content_chunks": [abstract]
+    }
